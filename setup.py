8247cbb53b5ae8a2998152e8ae54d381f56afd97
Masashi.Kayahara <masashi.kayahara@arc-connects.com>
2022-08-31 20:08:31 +0900

    fix findpackage

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='python_logger_core_sample',
     version='0.0.2',
-    packages=[''],
+    packages=setuptools.find_packages(),
     url='https://github.com/masakaya/python_logger_core_sample',
     license='MIT',
     author='Masashi Kayahara',
