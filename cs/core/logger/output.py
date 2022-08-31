7b852d311b7b0accf392f34353b6591855b01dfb
Masashi.Kayahara <masashi.kayahara@arc-connects.com>
2022-08-31 19:38:20 +0900

    Add Sample.

--- cs/core/logger/output.py
+++ cs/core/logger/output.py
@@ -1,7 +1,8 @@
 import json
 
 
-def output(message):
+def out(message):
     data = json.loads(message)
     json_message = json.dumps(data, indent=2)
+    print(json_message)
     return json_message
---
+++ sample/output.py
@@ -0,0 +1,4 @@
+from cs.core.logger import output
+
+if __name__ == '__main__':
+    output.out('{"aa":"test"}')
