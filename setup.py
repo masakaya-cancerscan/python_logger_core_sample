fcb499446ed1b82ff58129292cb245e6ddb9e65d
Masashi.Kayahara <masashi.kayahara@arc-connects.com>
2022-08-31 17:39:30 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='python-logger-core-sample',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/masakaya/python-logger-core-sample',
+    license='MIT',
+    author='Masashi Kayahara',
+    author_email='masashi.kayahara@arc-connects.com',
+    description='',
+    python_requires=">=3.7",
+)
