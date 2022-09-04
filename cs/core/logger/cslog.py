4f985636de8992dae139420d06be6e487d816b32
Masashi.Kayahara <masashi.kayahara@arc-connects.com>
2022-09-04 15:01:08 +0900

    Add sample.

--- cs/core/logger/cslog.py
+++ cs/core/logger/cslog.py
@@ -1,29 +1,20 @@
-import logging
 import logging.config
-from enum import Enum
-from pprint import pprint
-import google.cloud.logging
-import cs.core.utils.config
-import yaml
-import os
-
 from cs.core.utils.config import AppConfig
-from google.cloud.logging import Client
-from google.cloud.logging.handlers import CloudLoggingHandler
-from google.oauth2 import service_account
 
 
 class CsLogger:
     def __init__(self, config_file=''):
         self.config_file = config_file
 
-    def get_logger(self, name, log_type='default'):
+    def get_logger(self, name, log_type='develop'):
 
         if not self.config_file:
             log_config = AppConfig.get_config_dict("logging_config.yml")
         else:
             log_config = AppConfig.get_config_dict(self.config_file)
 
+        # 環境変数(environment)
+
         logging.config.dictConfig(log_config)
-        logger = logging.getLogger(log_type + "." + name)
+        logger: logging.Logger = logging.getLogger(log_type + "." + name)
         return logger
--- sample/cslog.py
+++ sample/cslog.py
@@ -3,6 +3,9 @@ from cs.core.logger.cslog import CsLogger
 
 logger = CsLogger().get_logger(__name__)
 
+def zero_division():
+    1/0
+
 if __name__ == '__main__':
     # 'application' code
     logger.debug('debug message')
@@ -11,8 +14,13 @@ if __name__ == '__main__':
     logger.error('error message')
     logger.critical('critical message')
 
-    # logger1.debug('debug message')
-    # logger1.info('info message')
-    # logger1.warning('warn message')
-    # logger1.error('error message')
-    # logger1.critical('critical message')
+    # コンソールは出力されないが、Google Cloud Loggingには自動的に出力される
+    custom_dict = {
+        'custom1':"test1"
+    }
+    logger.info('custom value',extra=custom_dict)
+
+    try:
+        zero_division()
+    except Exception as ex:
+        logger.error(ex, exc_info=True)
